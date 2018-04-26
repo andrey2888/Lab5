80debaed3ed30d660a003d84f5151569b720df58
andrey <>
2018-04-26 13:57:22 +0000

    Lab5

--- dead.c
+++ dead.c
@@ -10,6 +10,10 @@
 
 pthread_mutex_t lock1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t lock2 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock3 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock4 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock5 = PTHREAD_MUTEX_INITIALIZER;
+
 
     void *function1()
     {
@@ -26,20 +30,59 @@ pthread_mutex_t lock2 = PTHREAD_MUTEX_INITIALIZER;
 
            pthread_mutex_lock(&lock2); 
            sleep(1);
-           pthread_mutex_lock(&lock1);
+           pthread_mutex_lock(&lock3);
            printf("second\n");
-           pthread_mutex_unlock(&lock1);
+           pthread_mutex_unlock(&lock3);
            pthread_mutex_unlock(&lock2);
         
     } 
-  
+    void *function3(){
+
+           pthread_mutex_lock(&lock3); 
+           sleep(1);
+           pthread_mutex_lock(&lock4);
+           printf("third\n");
+           pthread_mutex_unlock(&lock4);
+           pthread_mutex_unlock(&lock3);
+        
+    }
+    void *function4(){
+
+           pthread_mutex_lock(&lock4); 
+           sleep(1);
+           pthread_mutex_lock(&lock5);
+           printf("fourth\n");
+           pthread_mutex_unlock(&lock5);
+           pthread_mutex_unlock(&lock4);
+        
+    } 
+    
+    void *function5(){
+
+           pthread_mutex_lock(&lock5); 
+           sleep(1);
+           pthread_mutex_lock(&lock1);
+           printf("fifth\n");
+           pthread_mutex_unlock(&lock1);
+           pthread_mutex_unlock(&lock5);
+        
+    } 
     int main()
     {
        pthread_t thread1;
        pthread_t thread2;
+       pthread_t thread3;
+       pthread_t thread4;
+       pthread_t thread5;
        pthread_create(&thread1, NULL, function1, NULL);
        pthread_create(&thread2, NULL, function2, NULL);
+       pthread_create(&thread3, NULL, function3, NULL);
+       pthread_create(&thread4, NULL, function4, NULL);   
+       pthread_create(&thread5, NULL, function5, NULL);
     
     pthread_join(thread1,0);
     pthread_join(thread2,0);
+    pthread_join(thread3,0);
+    pthread_join(thread4,0);
+    pthread_join(thread5,0);
     }
--- factorial.c
+++ factorial.c
@@ -14,15 +14,10 @@ struct FactArgs {
   int k;
 };
 
-static long fact_global = 1;
+long fact_global = 1;
 pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
 
-void multiply_fact(int a, int k){
-    pthread_mutex_lock(&mut);
-    fact_global *= a;
-    fact_global %= k;
-    pthread_mutex_unlock(&mut);
-}
+void multiply_fact(int a, int k);
 
 void *fact_from_to(void* args);
 
@@ -37,11 +32,11 @@ while (1) {
 
     static struct option options[] = {{"mod", required_argument, 0, 0},
                                       {"threads_num", required_argument, 0, 0},
-                                      {"argument", required_argument, 0, 0},
+                                      //{"argument", required_argument, 0, 0},
                                       {0, 0, 0, 0}};
 
     int option_index = 0;
-    int c = getopt_long(argc, argv, "k", options, &option_index);
+    int c = getopt_long(argc, argv, ":k:", options, &option_index);
 
     if (c == -1) break;
 
@@ -62,17 +57,19 @@ while (1) {
                 return 1;
             }
             break;
-          case 2:
+          /*case 2:
             n = atoi(optarg);
             if (n <= 0) {
                 printf("threads_num is a positive number\n");
                 return 1;
             }
-            break;
+            break;*/
           defalut:
             printf("Index %d is out of options\n", option_index);
         }
         break;
+      case 'k':
+        n = atoi(optarg);
       case '?':
         break;
 
@@ -92,6 +89,10 @@ fflush(NULL);
 pthread_t threads[threads_num];
 struct FactArgs  args[threads_num];
 
+struct timeval start_time;
+struct timeval fin_time;
+gettimeofday(&start_time, NULL);
+  
 int step = n/threads_num;
 printf("n %i\n",n);
 printf("num %i\n",threads_num);
@@ -109,20 +110,37 @@ printf("step %i\n",step);
    pthread_join(threads[i], NULL);
    }
    printf("%ld\n",fact_global);
+  
+  gettimeofday(&fin_time, NULL);
+  double elapsed_time = (fin_time.tv_sec - start_time.tv_sec) * 1000.0;
+  elapsed_time += (fin_time.tv_usec - start_time.tv_usec) / 1000.0;
+  printf("Elapsed time: %fms\n", elapsed_time);
+   
+}
+
+
+void multiply_fact(int a, int k){
+    //pthread_mutex_lock(&mut);
+    fact_global *= a;
+    fact_global %= k;
+    //pthread_mutex_unlock(&mut);
 }
 
 void *fact_from_to(void* args){
+    pthread_mutex_lock(&mut);
     struct FactArgs* FA = args;
-    long partial_factorial = 1;
-   
-    
+    //long partial_factorial = 1;
+    long partial_factorial = fact_global;
     for(int i = FA->begin; i < FA->end; i++){
         partial_factorial *= i;
         partial_factorial %= FA->k; 
     }
-    printf("%i    %i    %ld\n", FA->begin, FA->end, partial_factorial);
-    //printf("partial factorial %ld\n", partial_factorial);
-    fflush(NULL);
-    multiply_fact(partial_factorial,FA->k);
+    
+    //printf("%i    %i    %ld\n", FA->begin, FA->end, partial_factorial);
+    
+    //multiply_fact(partial_factorial,FA->k);
+    fact_global = partial_factorial;
+    
+    pthread_mutex_unlock(&mut);
     return 0;
 }
